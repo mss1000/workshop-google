fb6966ffb2484c8c4e13a8291270e791cb3d4772
dawright22 <53329137+dawright22@users.noreply.github.com>
2019-12-12 16:27:54 +1100

    Update main.tf

--- gcp-kms-unseal/main.tf
+++ gcp-kms-unseal/main.tf
@@ -76,18 +76,18 @@ output "vault_server_instance_id" {
 }
 
 # Create a KMS key ring
-# resource "google_kms_key_ring" "key_ring" {
-#   project  = "${var.gcloud-project}"
-#   name     = "${var.key_ring}"
-#   location = "${var.keyring_location}"
-# }
+ resource "google_kms_key_ring" "key_ring" {
+  project  = "${var.gcloud-project}"
+   name     = "${var.key_ring}"
+   location = "${var.keyring_location}"
+ }
 
 # Create a crypto key for the key ring
-# resource "google_kms_crypto_key" "crypto_key" {
-#   name            = "${var.crypto_key}"
-#   key_ring        = "${google_kms_key_ring.key_ring.self_link}"
-#   rotation_period = "100000s"
-# }
+ resource "google_kms_crypto_key" "crypto_key" {
+   name            = "${var.crypto_key}"
+   key_ring        = "${google_kms_key_ring.key_ring.self_link}"
+   rotation_period = "100000s"
+ }
 
 # Add the service account to the Keyring
 resource "google_kms_key_ring_iam_binding" "vault_iam_kms_binding" {
