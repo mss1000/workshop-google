9a150670ded90e0df90be5324b017e528d4c26f7
dawright22 <53329137+dawright22@users.noreply.github.com>
2019-12-12 16:26:01 +1100

    Add files via upload

--- gcp-kms-unseal/main.tf
+++ gcp-kms-unseal/main.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = var.account_creds
+  credentials = file(var.account_file_path)
   project     = var.gcloud-project
   region      = var.gcloud-region
 }
@@ -76,22 +76,22 @@ output "vault_server_instance_id" {
 }
 
 # Create a KMS key ring
- resource "google_kms_key_ring" "key_ring" {
-   project  = "${var.gcloud-project}"
-   name     = "${var.key_ring}"
-   location = "${var.keyring_location}"
- }
+# resource "google_kms_key_ring" "key_ring" {
+#   project  = "${var.gcloud-project}"
+#   name     = "${var.key_ring}"
+#   location = "${var.keyring_location}"
+# }
 
- Create a crypto key for the key ring
- resource "google_kms_crypto_key" "crypto_key" {
-   name            = "${var.crypto_key}"
-  key_ring        = "${google_kms_key_ring.key_ring.self_link}"
-  rotation_period = "100000s"
- }
+# Create a crypto key for the key ring
+# resource "google_kms_crypto_key" "crypto_key" {
+#   name            = "${var.crypto_key}"
+#   key_ring        = "${google_kms_key_ring.key_ring.self_link}"
+#   rotation_period = "100000s"
+# }
 
 # Add the service account to the Keyring
 resource "google_kms_key_ring_iam_binding" "vault_iam_kms_binding" {
-  key_ring_id = "${google_kms_key_ring.key_ring.id}"
+  # key_ring_id = "${google_kms_key_ring.key_ring.id}"
   key_ring_id = "${var.gcloud-project}/${var.keyring_location}/${var.key_ring}"
   role = "roles/owner"
 
